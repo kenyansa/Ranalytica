ff7569266bd859720ab67743876843df1fbdc23c
Kenyansa <kenyansajustine@gmail.com>
2022-04-22 05:17:12 +0300

    add fetch code to the js file to test its functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+function rentData() {
+    console.log("---------------------------")
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "X-RapidAPI-Host",
+    " realtymole-rental-estimate-v1.p.rapidapi.com"
+  );
+  myHeaders.append(
+    "X-RapidAPI-Key",
+    " 34b4bff699mshb59a8acfe51d6dap1eac04jsn3074c3659394"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?propertyType=Apartment&address=5500 Grand Lake Drive, San Antonio, TX&bathrooms= 2&compCount= 5&squareFootage= 1600&bedrooms= 4",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
